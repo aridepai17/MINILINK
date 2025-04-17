7aa2c59ed48b2e86e5bf1b9447981e8ebed7166c
aridepai17 <advaithdepai26@gmail.com>
2025-04-17 18:15:34 +0530

    Changes

--- link-in-bio-app/src/pages/[slug].js
+++ link-in-bio-app/src/pages/[slug].js
@@ -1,38 +1,35 @@
-// pages/[slug].js
-
 export async function getServerSideProps({ params }) {
-    const { slug } = params;
-  
-    const connectDB = (await import("@/lib/connectDB")).default;
-    const userModel = (await import("@/model/userModel")).default;
-  
-    await connectDB();
-  
-    const user = await userModel.findOne({ "links.shortUrl": slug });
-  
-    if (!user) {
-      return {
-        notFound: true,
-      };
-    }
-  
-    const link = user.links.find((l) => l.shortUrl === slug);
-  
-    if (!link || !link.link) {
-      return {
-        notFound: true,
-      };
-    }
-  
+  const { slug } = params;
+
+  const connectDB = (await import("@/lib/connectDB")).default;
+  const userModel = (await import("@/model/userModel")).default;
+
+  await connectDB();
+
+  const user = await userModel.findOne({ "links.shortUrl": slug });
+
+  if (!user) {
     return {
-      redirect: {
-        destination: link.link, // redirects to the original URL the user submitted
-        permanent: false,
-      },
+      notFound: true,
     };
   }
-  
-  export default function RedirectPage() {
-    return null;
+
+  const link = user.links.find((l) => l.shortUrl === slug);
+
+  if (!link || !link.url) {
+    return {
+      notFound: true,
+    };
   }
-  
+
+  return {
+    redirect: {
+      destination: link.url,
+      permanent: false,
+    },
+  };
+}
+
+export default function RedirectPage() {
+  return null;
+}
--- link-in-bio-app/src/pages/_app.js
+++ link-in-bio-app/src/pages/_app.js
@@ -1,5 +1,5 @@
 import { SessionProvider } from "next-auth/react";
-import "@/styles/globals.css";
+import "@/styles/globals.css"; // Make sure this is the correct path to your global styles
 
 export default function App({ Component, pageProps }) {
   return (
